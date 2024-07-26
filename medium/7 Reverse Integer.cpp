b48da9cb11985fbcef215757577f6d6541ce981b
anushka1243 <102985257+anushka1243@users.noreply.github.com>
2024-07-26 18:39:01 +0530

    Update 7 Reverse Integer.cpp

--- medium/7 Reverse Integer.cpp
+++ medium/7 Reverse Integer.cpp
@@ -1,13 +1,19 @@
- int reverse(int x) {
-        long long result = 0;
-        while (x != 0) {
-            result = result * 10 + x % 10;
-            x /= 10;
+class Solution {
+public:
+     int reverse(int x) {
+       long long ans=0;
+       while(x!=0)
+        {
+            ans=ans*10 + x%10;
+            x=x/10;
         }
-        // Check for overflow and underflow
-        if (result > INT_MAX || result < INT_MIN) return 0;
-        return static_cast<int>(result);
+        if(ans> INT_MAX || ans < INT_MIN)
+          return 0;
+        else
+          return static_cast<int>(ans); 
     }
+};
+
 
 /*
 static_cast is use to convert from long long to int

0890ad6ae157024565be908f410587008b930dcf
anushka1243 <102985257+anushka1243@users.noreply.github.com>
2024-08-02 15:46:05 +0530

    Update 20. Valid Parenthesis.cpp

--- Easy/20. Valid Parenthesis.cpp
+++ Easy/20. Valid Parenthesis.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    bool isValid(string x) {
+       stack<char>s;
+        for(int i=0;i<x.length();i++)
+        {
+            if(!s.empty() && x[i]==')' && s.top()=='(')
+              s.pop();
+            else if(!s.empty() && x[i]==']' && s.top()=='[')
+              s.pop();  
+            else if(!s.empty() && x[i]=='}' && s.top()=='{')
+              s.pop();   
+            else
+              s.push(x[i]);
+        }
+        return s.empty(); 
+    }
+};

498e94b2a3efd4767e801c37e6346f0352a0dfb0
anushka1243 <102985257+anushka1243@users.noreply.github.com>
2024-08-02 15:42:40 +0530

    Update 14. longest common prefix.cpp

--- Easy/14. longest common prefix.cpp
+++ Easy/14. longest common prefix.cpp
@@ -1,1 +1,26 @@
-
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+       int z=-1;
+       string v="";
+       sort(strs.begin(),strs.end());
+       for(int i=0;i<strs[0].size();i++)
+       {
+            int j=1;
+            while(j<strs.size())
+            {
+                if(strs[0][i]!=strs[j][i])
+                {
+                    z=i;
+                    break;
+                }
+                j++;
+            }
+            if(z==i)break;
+            v+=strs[0][i];
+       } 
+        if(z==0)return "";
+        else
+          return v;
+    }
+};

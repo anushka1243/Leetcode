9643208d7e64ddd90978a1fade6f6d26b6acb388
anushka1243 <102985257+anushka1243@users.noreply.github.com>
2024-09-01 08:38:00 +0530

    Update 21. Merge Two Sorted Lists.cpp

--- Easy/21. Merge Two Sorted Lists.cpp
+++ Easy/21. Merge Two Sorted Lists.cpp
@@ -1,1 +1,33 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* dummy= new ListNode(); ;
+        ListNode* p=list1;
+        ListNode* q=list2;
+        ListNode* r=dummy;
+        while(p && q)
+        {
+            if(p->val < q->val)
+            {
+                r->next=p;
+                p=p->next;
+            }
+            else
+            {
+                r->next=q;
+                q=q->next;
+            }
+            r=r->next;
+        }
+        if(p)
+        {
+            r->next=p;
+        }
+        if(q)
+        {
+            r->next=q;
+        }
 
+        return dummy->next;
+    }
+};
